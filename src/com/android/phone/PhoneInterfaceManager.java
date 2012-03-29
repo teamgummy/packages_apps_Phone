316d77b2f603b456c181afcd3502e93c3f1ba366
Kejar31 <justin.a.rogers@gmail.com>
2012-03-28 21:34:21 -0500

    updated aosp repo to upp from 4.0.4

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -58,7 +58,6 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     private static final int CMD_ANSWER_RINGING_CALL = 4;
     private static final int CMD_END_CALL = 5;  // not used yet
     private static final int CMD_SILENCE_RINGER = 6;
-    private static final int CMD_TOGGLE_LTE = 7; // not used yet
 
     /** The singleton instance. */
     private static PhoneInterfaceManager sInstance;
@@ -280,18 +279,6 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mApp.startActivity(intent);
     }
-
-    public void LTEtoggle(boolean on) {
-      int network = -1;
-      if (on) {
-        network = Phone.NT_MODE_GLOBAL;
-      } else {
-        network = Phone.NT_MODE_CDMA;
-      }
-      mPhone.setPreferredNetworkType(network, mMainThreadHandler.obtainMessage(CMD_TOGGLE_LTE));
-      Settings.Secure.putInt(mApp.getContentResolver(), Settings.Secure.PREFERRED_NETWORK_MODE, network);
-    }
-
 
     private boolean showCallScreenInternal(boolean specifyInitialDialpadState,
                                            boolean initialDialpadState) {
